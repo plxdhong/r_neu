9bef8dabbcaf324294cc453b735aeb619e29ce16
plxdhong <plxdhong@outlook.com>
2020-09-29 02:23:09 +0800

    添加项目文件。

---
+++ r_neu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace r_neu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ r_neu/auth/Neu_user.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace r_neu.auth
+{
+    class Neu_user
+    {
+        private static readonly HttpClient HttpClient;
+
+        static Neu_user()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("AppSettings is empty.");
+                }
+                else
+                {
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine("Key: {0} Value: {1}", key, appSettings[key]);
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+            HttpClient = new HttpClient();
+        }
+    }
+}
